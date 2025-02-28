8d7952cbe00dfc0ef12ed66694a7dd63ea614984
sanjid <sanjidtalukder02@gmail.com>
2025-02-28 16:08:00 +0600

    commit for add card section when the user click on button

--- js/script.js
+++ js/script.js
@@ -48,3 +48,37 @@ for(let btn of quantityElements){
     });
 }
 
+// Add the card section js
+
+let cartCount = 0;
+let cartItems = [];
+document.getElementById("add-to-cart").addEventListener("click",function(){
+    const quantity = parseInt(document.getElementById("quantity").innerText);
+    if(quantity>0){
+        document.getElementById("checkout-container").classList.remove("hidden");
+        cartCount= cartCount+ quantity;
+        document.getElementById("cart-count").innerText = cartCount;
+        const selectedColorButton = document.querySelector("button.border-gray-600.w-6");
+        const selectedColor = selectedColorButton ? selectedColorButton.id.split("-")[0]:"S";
+        const selectedSizeButtons = document.querySelector("button.border-purple-600:not(.w-6)");
+        const selectedSize = selectedSizeButtons.innerText.split(" ")[0];
+    const selectedPrice = selectedSizeButtons.innerText
+      .split(" ")[1]
+      .split("$")[1];
+
+    cartItems.push({
+      image: selectedColor + ".png",
+      title: " Classy Modern Smart Watch",
+      color: selectedColor,
+      size: selectedSize,
+      quantity: quantity,
+      price: quantity * parseInt(selectedPrice),
+    });
+
+    console.log(cartItems);
+  } else {
+    alert("Please select a quantity...");
+  }
+ 
+});
+
